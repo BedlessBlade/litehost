e0b0bf0110ee44657dbd2f419f65c0b6360f8377
--global <--global>
2025-09-16 16:59:20 -0400

    added functionality

--- instance-manager/tmux.py
+++ instance-manager/tmux.py
@@ -1,6 +1,6 @@
 import subprocess; from constants import *
 
-def make_session(index):
+def start_session(index):
     name = dirname + index
     subprocess.run(["tmux", "new-session", "-d", "-s", name, base_path + name])
 
@@ -11,3 +11,11 @@ def stop_server(index):
 def announce(data, index):
     name = dirname + index
     subprocess.run(["tmux", "send-keys", "-t", f"{name}:0.0", "say " + data, "ENTER"])
+
+def announce_all(data, servers):
+    for i in range(len(servers)):
+        announce(data, i)
+
+def stop_all(servers):
+    for i in range(len(servers)):
+        stop_server(i)
