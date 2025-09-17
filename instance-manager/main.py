c18e928d874bda3004b28c392621545e7e2c0f06
BedlessBlade <jh515playz@gmail.com>
2025-09-16 22:03:43 -0400

    add many features

--- instance-manager/main.py
+++ instance-manager/main.py
@@ -1,9 +1,14 @@
-import socket, os; from constants import *
+import socket, os; from constants import *; from tmux import *; from util import *
 
-os.system(f'ssh -L {server_port}:127.0.0.1:{ssh_port} {ssh_user}@{ssh_hostname}')
-
+try:
+    os.system(f'ssh -L {server_port}:127.0.0.1:{ssh_port} {ssh_user}@{ssh_hostname}')
+except Exception as e:
+    print("SSH Tunnel Error: " + str(e))
 server = socket.socket()
-server.bind((ssh_hostname, server_port))
+try:
+    server.bind((ssh_hostname, server_port))
+except Exception as e:
+    print("Socket Bind Error: " + str(e))
 server.listen()
 
 conn, addr = server.accept()
@@ -13,6 +18,26 @@ while True:
     data = conn.recv(1024)
     if not data:
         break
-    if data.decode() == "":
-        #placeholder
+    args = data.decode().split(" ")
+    if len(args) > 0:
+        if data.decode()[0] == 'start':
+            start_server(args[1])
+        elif data.decode()[0] == 'stop':
+            stop_server(args[1])
+        elif data.decode()[0] == 'announce':
+            #todo: fix announce
+            pass
+        elif data.decode()[0] == 'create':
+            create_instance(args[1])
+        elif data.decode()[0] == 'destroy':
+            destroy_instance(args[1])
+        elif data.decode()[0] == 'update':
+            pumpkin_update()
+        elif data.decode()[0] == 'startall':
+            start_all(args[1])
+        elif data.decode()[0] == 'stopall':
+            stop_all(args[1])
+    conn.send(b'ack')
         
+            
+
--- instance-manager/util.py
+++ instance-manager/util.py
@@ -1,9 +1,14 @@
-import os; from constants import *
-
+import os; from constants import *; from tmux import stop_server
+ 
 def create_instance(index):
-    name = f"instances/{dirname} + {index}"
+    name = f"instances/{dirname + index}"
     os.system(f'mkdir {name}')
-    os.chdir(name)
+    os.system(f"ln -s pumpkin {name}/pumpkin")
+
+def destroy_instance(index):
+    stop_server(index)
+    name = f"instances/{dirname + index}"
+    os.system(f"rm {name}")
 
 def pumpkin_update():
     os.system(f"rm Pumpkin && git clone https://github.com/Pumpkin-MC/Pumpkin.git && cd Pumpkin && RUSTFLAGS='-C target-cpu=native' cargo build --release -j {compiling_threads}")
